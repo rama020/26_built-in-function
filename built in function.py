9ffead18d6f327b27edbb0d88d22e8375858cf10
rama020 <118764128+rama020@users.noreply.github.com>
2022-12-21 20:38:12 +0530

    Add files via upload

---
+++ built in function.py
@@ -0,0 +1,19 @@
+'''
+DATE:21TH DEC 2022
+DAY: TUESDAY
+TOPIC: BUILT  IN FUNCTION
+AUTHOR:RAMA BHARGAvi
+''' 
+a=[1,3,5,7]
+a.sort()
+print(2)
+print(sorted([12,4,9]))
+print((15,5,8))
+a=['b','B','j','J']
+print(sorted(a))
+print(all([True,1,2]))
+print(all([True,'',1,2]))
+print(all([True,False,1,2]))
+print(all([True,True,1,2,None]))
+print(any([True,True,None,0]))
+print(any([True,False,False,0,23]))
